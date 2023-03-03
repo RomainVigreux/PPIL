2a755cd62fb36483d1c928ce68ae8ac3c3a618b7
RomainVigreux <romainaline@live.fr>
2023-03-03 10:40:01 +0100

    Main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,19 @@ using namespace std;
 
 int main (){
     Shape * f = new Segment(Vecteur2D(1, 2), Vecteur2D(3, 4), "red");
-    VisitorSave * v = new VisitorSaveText();
-    f->save(v);
-    delete f;
-    delete v;
-    return 0;
+    Shape * f2 = new Cercle(Vecteur2D(1, 2), 3, "blue");
+    Shape * f3 = new Polygone(Vecteur2D(1, 2), Vecteur2D(3, 4), Vecteur2D(5, 6), "green");
+
+    GroupForm * g = new GroupForm();
+    g->add(f);
+    g->add(f2);
+    g->add(f3);
+
+    cout << "Cercle : " << f2 << endl;
+    f2->translation(Vecteur2D(1, 1));
+    cout << "Cercle : " << f2 << endl;
+
+    
 }
 
 
